1e0d8db9331738bf8bb248e64e6e678abc081033
armin busatlic <dzarma.busatlic@gmail.com>
2020-11-20 13:57:16 +0100

    meking changes on list_page Head component

--- src/pages/upcoming-events.js
+++ src/pages/upcoming-events.js
@@ -4,11 +4,33 @@ import Layout from "../components/layout/Layout"
 import Head from "../components/head/Head"
 import Section1 from "../components/listPageSections/listPageSection1/section1/Section1"
 import Section2 from "../components/listPageSections/listPageSection2/Section2"
-const List = () => {
+
+export const query = graphql`
+  query ListPageQuery {
+    allPrismicListPage {
+        edges {
+          node {
+            data {
+              meta_description
+              meta_keywords
+              meta_title
+              page_name {
+                text
+              }
+            }
+          }
+        }
+      }
+  }
+`
+const List = ({data}) => {
+console.log(data)
+const seoData = data.allPrismicListPage.edges[0].node.data
+
     return(
         <>
         <Layout>
-            <Head title="List-page"/>
+            <Head metaData={seoData}/>
             <main>
                 <Section1/>
                 <Section2 />
