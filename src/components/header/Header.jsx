7b3d98ae29f2ea8f72a70b6f313ddbc784304092
armin busatlic <dzarma.busatlic@gmail.com>
2021-02-11 16:40:14 +0100

    Changing logo from PNG to SVG

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import { Link } from "gatsby";
 import Menu from "../menu/Menu";
 import headerStyles from "./header.module.scss";
-// import Logo from "../../assets/EWENTIVE-logo-new.svg";
-import Logo from "../../images/EWENTIVE-logo-new.png";
+import Logo from "../../assets/EwentiveLogo3.svg";
+
 const Header = () => {
   return (
     <header className={headerStyles.header}>
       <div className={headerStyles.mobile__header}>
         <Link className={headerStyles.logo} to="/">
           <div className={headerStyles.header_logo_wrapper}>
-            <img className={headerStyles.logo} src={Logo} alt="logo" />
+            <Logo className={headerStyles.logo} />
           </div>
         </Link>
       </div>
